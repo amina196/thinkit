bdd843eeabae1fb06f07543a6541daff68b90db8
Amina Bouabdallah <amina.bouabdallah@gmail.com>
2013-05-18 10:58:18 -0400

    initialised repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thinkit::Application.config.secret_token = '05bcf2726e53a17ccc39c75050e2e3fc556219ab6fa42e6412a02a8227b0c37f8cc54e019d4d6bd7f9bc8bb4bc6756bbbc77b999168d47ffde94aa60552d356c'
